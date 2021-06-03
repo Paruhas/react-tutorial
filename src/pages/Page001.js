1bcc7693273c7df668a21c037204074256d18ccc
apbank2238 <bayulgang@hotmail.com>
2021-06-03 16:24:49 +0700

    useState Array Add

--- src/pages/Page001.js
+++ src/pages/Page001.js
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import "./css/Page001.css";
 
+const clearInput = {
+  input1: "",
+  input2: "",
+};
+
 function Page001() {
-  const [input, setInput] = useState({
-    input1: "",
-    input2: "",
-  });
+  const [input, setInput] = useState(clearInput);
+  const [inputArray, setInputArray] = useState([]);
 
   const onInputChange = (event) => {
     const { name, value } = event.target;
@@ -17,6 +20,22 @@ function Page001() {
     }));
   };
 
+  const inputSubmit = (event) => {
+    event.preventDefault();
+    const newInput = { ...input, id: Date.now().toString() };
+    setInputArray((prev) => [newInput, ...prev]);
+    setInput(clearInput);
+  };
+
+  const inputElement = inputArray.map((item, index) => {
+    return (
+      <div className="inputArray" key={item.id}>
+        <p>Input1 Text: {item.input1}</p>
+        <p>Input2 Text: {item.input2}</p>
+      </div>
+    );
+  });
+
   return (
     <div>
       <Navbar />
@@ -24,23 +43,28 @@ function Page001() {
       <h1>USE-STATE</h1>
       <section className="app-section">
         <div className="app-container">
-          <div className="input-container">
-            <textarea
-              rows="5"
-              name="input1"
-              value={input.input1}
-              onChange={onInputChange}
-            />
-            <input
-              type="text"
-              name="input2"
-              value={input.input2}
-              onChange={onInputChange}
-            />
-          </div>
-          <div className="input-container">
-            <p>Input1 Text: {input.input1}</p>
-            <p>Input2 Text: {input.input2}</p>
+          <div>
+            <div className="input-container">
+              <h3>EXAMPLE TEXT</h3>
+              <p>Input1 Text: {input.input1}</p>
+              <p>Input2 Text: {input.input2}</p>
+            </div>
+            <form onSubmit={inputSubmit}>
+              <textarea
+                rows="5"
+                name="input1"
+                value={input.input1}
+                onChange={onInputChange}
+              />
+              <input
+                type="text"
+                name="input2"
+                value={input.input2}
+                onChange={onInputChange}
+              />
+              <button>+ ADD</button>
+            </form>
+            <div className="inputArray-container">{inputElement}</div>
           </div>
         </div>
       </section>
