1c4dfb547130232926304fafffc0adb9ab2de661
apbank2238 <bayulgang@hotmail.com>
2021-06-03 17:26:26 +0700

    useState Array delete edit

--- src/pages/Page001.js
+++ src/pages/Page001.js
@@ -9,8 +9,10 @@ const clearInput = {
 
 function Page001() {
   const [input, setInput] = useState(clearInput);
+  const [inputEdit, setInputEdit] = useState(null);
   const [inputArray, setInputArray] = useState([]);
 
+  // FNs input
   const onInputChange = (event) => {
     const { name, value } = event.target;
 
@@ -20,6 +22,16 @@ function Page001() {
     }));
   };
 
+  const onInputEditChange = (event) => {
+    const { name, value } = event.target;
+
+    setInputEdit((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  // FNs add
   const inputSubmit = (event) => {
     event.preventDefault();
     const newInput = { ...input, id: Date.now().toString() };
@@ -27,14 +39,71 @@ function Page001() {
     setInput(clearInput);
   };
 
-  const inputElement = inputArray.map((item, index) => {
+  // FNs delete
+  const inputDelete = (inputId) => {
+    setInputArray((prev) => prev.filter((item) => item.id !== inputId));
+  };
+
+  // FNs edit
+  const inputEditSubmit = (event) => {
+    event.preventDefault();
+
+    // เอา previousValue ของแต่ละ index ใน Array มา map
+    // แล้วเช็คว่า id ของแต่ละตัวใน "inputArray"(prev) ตรงกับ "inputEdit"
+    // ถ้าไม่ตรง return ค่าเดิมของมัน ถ้าตรง (===ตัวที่เรากดEditมา) ให้แทนค่าใหม่ที่Editเข้าไป
+    setInputArray((prev) => {
+      return prev.map((item) => {
+        if (item.id !== inputEdit.id) {
+          return item;
+        }
+        return inputEdit;
+      });
+    });
+
+    setInputEdit(null);
+  };
+
+  // ELEMENTs
+  const inputElement = inputArray.map((item) => {
     return (
       <div className="inputArray" key={item.id}>
         <p>Input1 Text: {item.input1}</p>
         <p>Input2 Text: {item.input2}</p>
+        <span className="inputArray-btn-grp">
+          <button onClick={() => setInputEdit(item)}>EDIT</button>
+          <button onClick={() => inputDelete(item.id)}>DELETE</button>
+        </span>
       </div>
     );
   });
+
+  let inputEditElement = null;
+  if (inputEdit) {
+    inputEditElement = (
+      <div className="editInput">
+        <div className="editInput-bg" onClick={() => setInputEdit(null)}></div>
+        <div className="editInput-content">
+          <form onSubmit={inputEditSubmit}>
+            <p>Input1 Text: </p>
+            <textarea
+              rows="5"
+              name="input1"
+              value={inputEdit.input1}
+              onChange={onInputEditChange}
+            />
+            <p>Input2 Text: </p>
+            <input
+              type="text"
+              name="input2"
+              value={inputEdit.input2}
+              onChange={onInputEditChange}
+            />
+            <button>SUBMIT EDIT</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -68,6 +137,7 @@ function Page001() {
           </div>
         </div>
       </section>
+      {inputEditElement}
     </div>
   );
 }
