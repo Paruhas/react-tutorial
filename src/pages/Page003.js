f80ae4ca77f5dfde90228b99ddeef8e15ab0b31a
apbank2238 <bayulgang@hotmail.com>
2021-06-06 21:56:01 +0700

    useReducer replace useState

--- src/pages/Page003.js
+++ src/pages/Page003.js
@@ -5,9 +5,18 @@ import "./css/Page003.css";
 
 export const AuthContext = createContext();
 
-function Page003() {
-  const [userAuth, setUserAuth] = useState(null);
+const authReducer = (state, action) => {
+  if (action.type === "login") {
+    return;
+  }
+  if (action.type === "logout") {
+    return;
+  }
+};
 
+const authInitialState = null;
+
+function Page003() {
   const [state, dispatch] = useReducer(authReducer, authInitialState);
 
   return (
@@ -15,7 +24,7 @@ function Page003() {
       <Navbar />
       <h1>PAGE003</h1>
       <h1>useContext</h1>
-      <AuthContext.Provider value={{ userAuth, setUserAuth }}>
+      <AuthContext.Provider value={{}}>
         <section className="app-section">
           <div className="app-container">
             <LoginArea />
