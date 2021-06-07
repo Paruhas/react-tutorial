b850ac802df270b333f4389d9ce801075842d08e
apbank2238 <bayulgang@hotmail.com>
2021-06-07 20:01:37 +0700

    useRef(1)

--- src/pages/Page004.js
+++ src/pages/Page004.js
@@ -5,6 +5,14 @@ import "./css/Page004.css";
 function Page004() {
   const fileRef = useRef(null);
 
+  const uploadFileChange = (event) => {
+    fileRef.current = event.target.files[0];
+  };
+
+  const submitUploadFile = (event) => {
+    console.log(fileRef.current);
+  };
+
   return (
     <div>
       <Navbar />
@@ -15,9 +23,9 @@ function Page004() {
           <div className="upload-file-box">
             <h3>UPLOAD FILE</h3>
             <p>
-              <input type="file" />
+              <input type="file" onChange={uploadFileChange} />
             </p>
-            <button>UPLOAD</button>
+            <button onClick={submitUploadFile}>UPLOAD</button>
           </div>
         </div>
         <div className="app-container">
